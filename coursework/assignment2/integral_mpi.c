00693fcd2e2489d4f2cd809b7ceb376eb7519bf9
Arthur Zopellaro <arthurazsoares@gmail.com>
2017-09-23 02:30:55 -0300

    Fix code not allocating all processes
    Also some other fixes such as:
    - Use double instead of float for data precision
    - Remove unnecessary variables

--- coursework/assignment2/integral_mpi.c
+++ coursework/assignment2/integral_mpi.c
@@ -1,61 +1,72 @@
 #include <stdio.h>
-#include <mpi.h>
 #include <math.h>
+#include <mpi.h>
 
-float f(float x) {
-    return 1/x;
-}
+double f(double x) { return 1 / x; }
 
-float calculate(float local_a, float local_b, int local_n, float h) {
+double calculate(double local_a, double local_b, int local_n, double h){
 
-    float x = local_a;
-    float integral = (f(local_a) + f(local_b)) / 2.0;
+    double x = local_a;
+    double integral = (f(local_a) + f(local_b)) / 2.0;
 
-    for (int i = 1; i <= local_n - 1; i++) {
+    for (int i = 1; i < local_n; i++) {
         x = x + h;
         integral = integral + f(x);
     }
 
     integral = integral * h;
-
     return integral;
 }
 
 int main(int argc, char *argv[]) {
 
-    int numprocs, rank, tag = 0, master = 0;
+    int numprocs, rank, master = 0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
     // Integral (1 to 6) of 1/x
-    float a = 1, b = 6, h;
-    int trapezoids = 2000;
-    h = (b-a)/trapezoids;
+    int a = 1, b = 6;
+    double h;
+    int trapezoids = 1237;
+    h = (double)(b-a)/trapezoids;
 
     // Local Variables
-    //TODO Fix local_n (it HAS to be = 2000, not < 2000)
-    float local_n = trapezoids / numprocs;
-    float local_a = a + rank * local_n * h;
-    float local_b = local_a + local_n * h;
+    int local_n = trapezoids / numprocs;  //if trapezoids not divisible
+    int extra = trapezoids % numprocs;    //by numprocs, split the rest
+    if (extra > rank) {                   //to other processes.
+        local_n++;                        //first few processes get the
+    }                                     //extra elements
+
+    double local_a = a + rank * local_n * h;
+    if (extra != 0 && extra <= rank) {
+        //recalculate local_a
+        //if there are processes
+        //with extra elements
+        local_a = local_a + (extra * h);
+    }
+    double local_b = local_a + local_n * h;
 
     // Calculate
-    float integral_local = calculate(local_a, local_b, local_n, h);
-    float integral;
+    double integral_local = calculate(local_a, local_b, local_n, h);
+    double integral;
 
-    MPI_Reduce(&integral_local, &integral, 1, MPI_FLOAT, MPI_SUM, master, MPI_COMM_WORLD);
+    MPI_Reduce(
+        &integral_local, &integral,
+        1, MPI_DOUBLE, MPI_SUM,
+        master, MPI_COMM_WORLD);
 
     if (rank == master) {
-        float expected = 1.79175946923;
+        double expected = 1.79175946923;
 
-        printf("Integral (%.0f to %.0f) of 1/x \n\n", a, b);
+        printf("Integral (%d to %d) of 1/x \n\n", a, b);
         printf("Expected  result  = %.11f\n", expected);
         printf("Estimated result  = %.11f\n", integral);
         printf("                    -------------\n");
 
-        float difference = expected - integral;
-        difference = fabsf(difference); // absolute value (no minus sign)
+        double difference = expected - integral;
+        difference = fabsf(difference); //absolute value (no minus sign)
         printf("Difference        = %.11f\n", difference);
     }
 
